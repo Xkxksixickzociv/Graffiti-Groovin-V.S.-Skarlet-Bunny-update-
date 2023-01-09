88e6b6cdeb89e993fc683c4fb913708b5d59de9c
Xkxksixickzociv <119426594+Xkxksixickzociv@users.noreply.github.com>
2023-01-09 23:35:23 +0200

    Add files via upload

---
+++ assets/preload/data/bunboxxin/modchart.lua
@@ -0,0 +1,13 @@
+
+  
+function onSongStart()
+   noteTweenX("bf1", 4, 86, 0.000001, cubeInOut)
+     noteTweenX("bf2", 5, 200, 0.000001, cubeInOut)
+     noteTweenX("bf3", 6, 312, 0.000001, cubeInOut)
+     noteTweenX("bf4", 7, 426, 0.000001, cubeInOut)
+
+     noteTweenX("dad1", 0, 740, 0.000001, cubeInOut)
+     noteTweenX("dad2", 1, 850, 0.000001, cubeInOut)
+     noteTweenX("dad3", 2, 960, 0.000001, cubeInOut)
+     noteTweenX("dad4", 3, 1070, 0.000001, cubeInOut)
+ end
